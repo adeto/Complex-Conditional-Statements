f0bb5ece24401e5ba9b1b6baacabaef24ba91ca4
adeto <adeto@users.noreply.github.com>
2017-01-21 13:49:24 +0200

    Cinema

---
+++ Cinema.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _12.Cinema
+{
+    class Cinema
+    {
+        static void Main()
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            decimal costPr = 12.00m;
+            decimal costNorm = 7.50m;
+            decimal costDisc = 5.00m;
+            decimal income;
+
+            if (type == "Premiere")
+            {
+                income = rows * cols * costPr;
+            }
+            else if (type == "Normal")
+            {
+                income = rows * cols * costNorm;
+            }
+            else
+            {
+                income = rows * cols * costDisc;
+            }
+            Console.WriteLine("{0:f2}", income + " leva");
+        }
+    }
+}
