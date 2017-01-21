e047b79863448a49ba73f02eb8a48ca826623ab1
adeto <adeto@users.noreply.github.com>
2017-01-21 13:50:29 +0200

    Small Shop

---
+++ Shop.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Small_Shop
+{
+    class Shop
+    {
+        static void Main()
+        {
+            var product = Console.ReadLine().ToLower();
+            var city = Console.ReadLine().ToLower();
+            var quantaty = double.Parse(Console.ReadLine());
+
+            if (city == "sofia")
+            {
+                if (product == "coffee")
+                    Console.WriteLine(quantaty * 0.50);
+            }
+            if (city == "sofia")
+
+            {
+                if (product == "water") Console.WriteLine(quantaty * 0.80);
+            }
+
+            if (city == "sofia")
+            {
+                if (product == "beer") Console.WriteLine(quantaty * 1.20);
+            }
+            if (city == "sofia")
+            {
+                if (product == "sweets") Console.WriteLine(quantaty * 1.45);
+            }
+            if (city == "sofia")
+            {
+                if (product == "peanuts") Console.WriteLine(quantaty * 1.60);
+            }
+
+            if (city == "plovdiv")
+            {
+                if (product == "coffee")
+                    Console.WriteLine(quantaty * 0.40);
+            }
+            if (city == "plovdiv")
+            {
+                if (product == "water") Console.WriteLine(quantaty * 0.70);
+            }
+            if (city == "plovdiv")
+            {
+                if (product == "beer") Console.WriteLine(quantaty * 1.15);
+            }
+            if (city == "plovdiv")
+            {
+                if (product == "sweets") Console.WriteLine(quantaty * 1.30);
+            }
+            if (city == "plovdiv")
+            {
+                if (product == "peanuts") Console.WriteLine(quantaty * 1.50);
+            }
+
+            if (city == "varna")
+            {
+                if (product == "coffee")
+                    Console.WriteLine(quantaty * 0.45);
+            }
+            if (city == "varna")
+            {
+                if (product == "water") Console.WriteLine(quantaty * 0.70);
+            }
+            if (city == "varna")
+            {
+                if (product == "beer") Console.WriteLine(quantaty * 1.10);
+            }
+            if (city == "varna")
+            {
+                if (product == "sweets") Console.WriteLine(quantaty * 1.35);
+            }
+            if (city == "varna")
+            {
+                if (product == "peanuts") Console.WriteLine(quantaty * 1.55);
+            }
+        }
+    }  
+}
