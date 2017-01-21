e865cfb3438e66ac8f49430ba17b4f2fe7bd3187
adeto <adeto@users.noreply.github.com>
2017-01-21 13:56:16 +0200

    Point on Segment

---
+++ PointOnSegment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace _03.Point_on_Segment
+{
+    class Program
+    {
+        static void Main()
+        {
+            var first = int.Parse(Console.ReadLine());
+            var second = int.Parse(Console.ReadLine());
+            var point = int.Parse(Console.ReadLine());
+
+
+            if (point <= first && point >= second)
+            {
+                
+                Console.Write("in ");
+                var inResult = (point - second);
+                Console.WriteLine(Math.Abs(inResult));
+            }
+            else
+            {
+                Console.Write("out ");
+                var outResult = (first - point);
+                Console.WriteLine(Math.Abs(outResult));
+                ;
+            }
+      
+        }
+    }
+}
