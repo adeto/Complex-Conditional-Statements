9da81bb9c316fa1169d7bd6225376a25338db27d
adeto <adeto@users.noreply.github.com>
2017-01-21 13:47:00 +0200

    Fruits or Vegetable

---
+++ FrOrVeg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04.Fruit_Of_Vegetables
+{
+    class FrOrVeg
+    {
+        static void Main()
+        {
+            var text = Console.ReadLine().ToLower();
+
+            if (text == "banana" || text == "apple" || text == "kiwi" || text == "cherry" || text == "lemon" || text == "grapes")
+            {
+
+                Console.WriteLine("fruit");
+            }
+
+        
+            else if (text == "tomato" || text == "cucumber" || text == "pepper" || text == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            
+           }
+           
+                else 
+            { 
+                Console.WriteLine("unknown");
+            }
+        }
+
+    }
+}
