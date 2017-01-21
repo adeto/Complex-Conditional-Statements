57a4e38ed59eeb727cb1823138588aafdbd0d43f
adeto <adeto@users.noreply.github.com>
2017-01-21 13:47:18 +0200

    Invalid Number

---
+++ InvalidNum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05.Invalid_Number
+{
+    class InvalidNum
+    {
+        static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            var validNum = (num == 0 || (num >= 100 && num <= 200));
+
+            if (!validNum)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
