97c5a635a40fdf61c3e2b66aa27aae94b0573268
adeto <adeto@users.noreply.github.com>
2017-01-21 13:46:12 +0200

    Gender and Age

---
+++ GenderAndAge.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace _01.Personal_Titles
+{
+    class GenderAndAge
+    {
+        static void Main()
+        {
+            var age = double.Parse(Console.ReadLine());
+            var gender = Console.ReadLine();
+
+            if (age >= 16 && gender == "m")
+            {
+                Console.WriteLine("Mr.");
+
+            }
+            if (age >= 16 && gender == "f" )
+            {
+                Console.WriteLine("Ms.");
+            }
+            else if (age < 16 && gender == "m")
+            {
+                Console.WriteLine("Master");
+            }
+            else if (age <16 && gender == "f")
+            {
+                Console.WriteLine("Miss");
+            }
+                
+        }
+    }
+}
