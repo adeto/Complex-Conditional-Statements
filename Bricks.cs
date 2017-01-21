9e0ddda26c1936ac324c024e1f94aaad48c31679
adeto <adeto@users.noreply.github.com>
2017-01-21 13:55:47 +0200

    Bricks

---
+++ Bricks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02.Bricks
+{
+    class Bricks
+    {
+        static void Main()
+        {
+            var x = int.Parse(Console.ReadLine());
+            var w = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+
+            double covarage = w * m;
+            double course = (x / covarage);
+
+            Console.WriteLine(Math.Ceiling(course));
+            
+            
+        }
+    }
+}
