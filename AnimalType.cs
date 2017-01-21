e1da903277376e8078ed1e61917e17141bcdf91c
adeto <adeto@users.noreply.github.com>
2017-01-21 13:49:07 +0200

    Animal Type

---
+++ AnimalType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10.Animal_Type
+{
+    class Animal
+    {
+        static void Main()
+        {
+            var text = Console.ReadLine();
+            switch (text)
+            {
+                case "dog": Console.WriteLine("mammal");break;
+                case "crocodile": Console.WriteLine("reptile");break;
+                case "tortoise": Console.WriteLine("reptile");break;
+                case "snake": Console.WriteLine("reptile");break;
+                default: Console.WriteLine("unknown"); break;
+            }
+        }
+    }
+}
