70af9adb0bff180933a4ef25d73d0e08f2f0998e
adeto <adeto@users.noreply.github.com>
2017-01-21 13:51:02 +0200

    Volayball

---
+++ Volayball.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volayball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+
+class Volleyball
+        {
+            static void Main()
+            {
+                string leap = Console.ReadLine();
+                int hollidaysCount = int.Parse(Console.ReadLine());
+                int weekendsHomeCount = int.Parse(Console.ReadLine());
+
+                int weekendsInYear = 48;
+                int normalWeekendsCount = weekendsInYear - weekendsHomeCount;
+                double gamesCount =
+                    normalWeekendsCount * 3.0 / 4.0 +
+                    weekendsHomeCount * 1.0 +
+                    hollidaysCount * 2.0 / 3.0;
+                if (leap == "leap")
+                {
+                    gamesCount = gamesCount * 1.15;
+                }
+                Console.WriteLine((int)gamesCount);
+            }
+        }
+
+    }
+}
+}
